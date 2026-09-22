8fa69bde21affc03d21b84fc34e6a09bfde8c9be
nihalgd <nihalgoud.12345@gmail.com>
2026-09-22 15:11:08 +0530

    Task 1

--- README.md
+++
@@ -1,2 +0,0 @@
-here is the deploy link
-https://work-seven-rose.vercel.app/
---
+++ Task 1/script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("alertButton");
+
+button.addEventListener("click", function () {
+    alert("Great job! You have successfully added JavaScript interactivity to your webpage.");
+});
